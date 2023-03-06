a14f7d5230fe7fd1a723ad6dd7ccde384c6253c0
Roberto Ramirez <robert.624@hotmail.com>
2023-03-06 10:19:01 -0500

    form desktop design finished

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,39 +1,62 @@
-import React, { useState } from 'react'
-import "./index.css"
+import React, { useState } from "react";
+import "./index.css";
 
 const Form = () => {
+    const [cardInfo, setCardInfo] = useState({});
 
-const [cardInfo, setCardInfo] = useState({})
+    function formSubmit(e) {}
 
-    function formSubmit(e){
-
-    }
-
-  return (
-    <div className='form-container'>
-        <form action="">
-            <label htmlFor="cardholder-name">
-                CARDHOLDER NAME
-                <input type="text" name='cardholder-name' placeholder='e.g. Jane Appleseed'/>
-            </label>
-            <label htmlFor="card-number">
-                CARD NUMBER
-                <input type="text" placeholder='e.g. 1234 5678 9123 0000' name='card-number'/>
-            </label>
-            <div className='card-additional-info'>
-                <label htmlFor="exp-date">
-                    EXP. DATE (MM/YY)
-                    <input type="number" placeholder='MM' name="month" id="month" />
-                    <input type="number" placeholder='YY' name="month" id="year" />
+    return (
+        <div className="form-container">
+            <form action="">
+                <label htmlFor="cardholder-name">
+                    CARDHOLDER NAME
+                    <input
+                        type="text"
+                        name="cardholder-name"
+                        placeholder="e.g. Jane Appleseed"
+                    />
                 </label>
-                <label htmlFor="CVC">
-                    <input type="number" placeholder='e.g. 123' name="cvc" id="cvc" />
+                <label htmlFor="card-number">
+                    CARD NUMBER
+                    <input
+                        type="text"
+                        placeholder="e.g. 1234 5678 9123 0000"
+                        name="card-number"
+                    />
                 </label>
-            </div>
-            <button type='submit'>Submit</button>
-        </form>
-    </div>
-  )
-}
+                <div className="card-additional-info">
+                    <div className="exp-date-inputs flex">
+                        <label htmlFor="exp-date">EXP. DATE (MM:YY)</label>
+                        <div className="inputs flex">
+                            <input
+                                type="number"
+                                placeholder="MM"
+                                name="month"
+                                id="month"
+                            />
+                            <input
+                                type="number"
+                                placeholder="YY"
+                                name="month"
+                                id="year"
+                            />
+                        </div>
+                    </div>
+                    <div className="exp-iputs">
+                        <label htmlFor="cvc">CVC</label>
+                        <input
+                            type="number"
+                            placeholder="e.g. 123"
+                            name="cvc"
+                            id="cvc"
+                        />
+                    </div>
+                </div>
+                <button type="submit">Confirm</button>
+            </form>
+        </div>
+    );
+};
 
-export default Form
+export default Form;
