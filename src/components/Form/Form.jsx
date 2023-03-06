271bca7726828f9c7d51842cb8ea3bf514c4765f
Roberto Ramirez <robert.624@hotmail.com>
2023-03-05 23:17:04 -0500

    first styles to form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.css"
 
 const Form = () => {
+
+const [cardInfo, setCardInfo] = useState({})
 
     function formSubmit(e){
 
     }
 
   return (
-    <div>
+    <div className='form-container'>
         <form action="">
             <label htmlFor="cardholder-name">
                 CARDHOLDER NAME
@@ -16,20 +18,19 @@ const Form = () => {
             </label>
             <label htmlFor="card-number">
                 CARD NUMBER
-                <input type="text" name='card-number'/>
+                <input type="text" placeholder='e.g. 1234 5678 9123 0000' name='card-number'/>
             </label>
-            <fieldset className='card-additional-info'>
+            <div className='card-additional-info'>
                 <label htmlFor="exp-date">
                     EXP. DATE (MM/YY)
-                    <input type="number" name="month" id="month" />
-                    <input type="number" name="month" id="year" />
+                    <input type="number" placeholder='MM' name="month" id="month" />
+                    <input type="number" placeholder='YY' name="month" id="year" />
                 </label>
-                <label htmlFor="exp-date">
-                    <input type="number" name="month" id="month" />
-                    <input type="number" name="month" id="year" />
+                <label htmlFor="CVC">
+                    <input type="number" placeholder='e.g. 123' name="cvc" id="cvc" />
                 </label>
-            </fieldset>
-
+            </div>
+            <button type='submit'>Submit</button>
         </form>
     </div>
   )
